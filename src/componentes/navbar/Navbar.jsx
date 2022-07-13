e8344aa9e542d15532d04e0528437d6f608d3c22
Xavier Guerrero <xavierignacioguerrero@gmail.com>
2022-07-13 15:02:07 -0300

    0.2 primeros pasos

---
+++ src/componentes/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import React from 'react'
+import './navbar.css'
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__list--item"> <a className="navbar__list--link" href='#'>Home</a> </li>
+        <li className="navbar__list--item"> <a className="navbar__list--link" href='#'>Nuestros Productos</a> </li>
+        <li className="navbar__list--item"> <a className="navbar__list--link" href='#'>Sobre Nosotros</a> </li>
+        <li className="navbar__list--item"> <a className="navbar__list--link" href='#'>Productos Especiales</a> </li>
+        <li className="navbar__list--item"> <a className="navbar__list--link" href='#'>Contacto</a> </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
